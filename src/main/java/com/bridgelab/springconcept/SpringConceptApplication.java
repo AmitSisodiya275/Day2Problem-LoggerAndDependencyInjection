eecd3c31bab673a8fec1f21689968fb901602932
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-12-12 18:00:21 +0530

    Added: two beans and done dependency injection using Autowired annotation.

--- src/main/java/com/bridgelab/springconcept/SpringConceptApplication.java
+++ src/main/java/com/bridgelab/springconcept/SpringConceptApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.bridgelab.springconcept.component.DemoBean;
+import com.bridgelab.springconcept.component.EmployeeBean;
 
 @SpringBootApplication
 public class SpringConceptApplication {
@@ -18,6 +19,11 @@ public class SpringConceptApplication {
 		DemoBean demoBean = context.getBean(DemoBean.class);
 		System.out.println(demoBean);
 		logger.debug("Demo Bean : "+context.getBean(DemoBean.class));
+		EmployeeBean employee = context.getBean(EmployeeBean.class);
+		employee.setEid(101);
+		employee.setEname("Amit");
+		employee.showDetails();
+		
 	}
 
 }
